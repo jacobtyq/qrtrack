5a7641fa9476c27b9920124b4290c80c93816d94
Jacob Tan <6391234+jacobtyq@users.noreply.github.com>
2025-04-15 18:09:10 +0800

    feat: Add Tailwind and ShadCn Navigation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -15,5 +17,8 @@ export default defineNuxtConfig({
       exclude: [],
       saveRedirectToCookie: false,
     },
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 })
